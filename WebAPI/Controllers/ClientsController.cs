d3fab71f2a2a863a9916fa7f04c7459a6a7974f0
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-09-18 23:44:29 -0300

    API finished
    Added a simple of PUT, POST and DELETE methods

---
+++ WebAPI/Controllers/ClientsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ClientsController : ApiController
+    {
+        private static List<Clients> Clients = new List<Clients>();
+
+        public List<Clients> Get()
+        {
+            return Clients;
+        }
+
+        public void Post(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+                Clients.Add(new Clients(name));
+        }
+
+        public void Delete(string name)
+        {
+            Clients.RemoveAt(Clients.IndexOf(Clients.First(x => x.name.Equals(name))));
+        }
+    }
+}
---
+++ WebAPI/Models/Clients.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Clients
+    {
+        public string name { get; set; }
+
+        public Clients(string name)
+        {
+            this.name = name;
+        }
+    }
+}
